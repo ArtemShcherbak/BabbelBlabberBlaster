ceb733978e2f86c75a6555bdc350942718cc82ca
Alexandre Tyrlik <atyrlik@babbel.com>
2019-08-01 11:41:14 +0200

    Create user input

--- app/src/main/java/com/babbel/blabberblaster/MainActivity.kt
+++ app/src/main/java/com/babbel/blabberblaster/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.babbel.blabberblaster
 
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.babbel.blabberblaster.model.Message
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import android.widget.Toast
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var messageHistoryAdapter: MessageHistoryAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +31,33 @@ class MainActivity : AppCompatActivity() {
             adapter = MessageHistoryAdapter().apply {
                 addMessage(Message("hello", true))
                 addMessage(Message("bye", false))
+                messageHistoryAdapter = this
             }
+        }
 
+        microphone.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH)
+            startActivityForResult(intent, 42)
+        }
+
+        send.setOnClickListener {
+            editText?.text?.let {
+                if (it.isNotEmpty()) {
+                    messageHistoryAdapter?.addMessage(Message(it.toString(), false))
+                    it.clear()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && data != null && requestCode == 42) {
+            val results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            messageHistoryAdapter?.addMessage(Message(results[0], false))
         }
     }
 }
